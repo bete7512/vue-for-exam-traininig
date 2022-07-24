818401596c896c9ed14088709a14c3d4ed4125ad
bete7512 <betekbebe@gmail.com>
2022-07-25 00:30:39 +0300

    here we go

---
+++ front-end-for-exam-traininig/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home flex text-red-700">
+    <img alt="Vue logo" class="flex justify-center items center mr-40" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
